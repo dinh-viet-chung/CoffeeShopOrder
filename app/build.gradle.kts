11f6a91ae7a5b8630c7c8ba3fab22bf871c11f05
dinh-viet-chung <125520234+dinh-viet-chung@users.noreply.github.com>
2025-08-19 11:24:25 +0700

    download environment
    realtime database  and create on firebase.googl.com ,
    import json
    trang rules to true and publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -37,10 +38,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     implementation(libs.glide)
     implementation(libs.gson)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-analytics")
 }
